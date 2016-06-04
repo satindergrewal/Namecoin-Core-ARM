faad729e1f91476013973eab345ff9dcd5912dc7
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-06-05 04:11:21 +1200

    update

--- install-nmc.sh
+++ install-nmc.sh
@@ -4,6 +4,6 @@ USER="$(whoami)"
 
 sudo cp -av bench_namecoin namecoin-cli namecoind namecoin-tx test_namecoin /usr/local/bin/
 sudo cp -av namecoin.service /etc/systemd/system/
-sed -i "s/root/${USER}/" /etc/systemd/system/namecoin.service
+sudo sed -i "s/root/${USER}/" /etc/systemd/system/namecoin.service
 sudo systemctl enable namecoin
 sudo systemctl start namecoin
